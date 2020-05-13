c4235a7c0d7e95f253d0247f4d8790ec4c5655d5
AishwaryaND <aishwaryadodmani88@gmail.com>
2020-05-13 23:37:29 +0530

    first commit

---
+++ reactnative/fontIOS/src/pages/home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native';
+
+import styles from './HomeStyle';
+
+const Home: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}
+        >
+          <View style={styles.body}>
+            <Text style={styles.welcome}>
+              Welcome to IBM Digital App Builder aaa
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default Home;
---
+++ reactnative/fontIOS/src/pages/home/HomeStyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#ffffff',
+    height: '100%'
+  },
+  body: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    height: '100%',
+    alignItems: 'center'
+  },
+  welcome: {
+    fontSize: 36,
+    fontWeight: '900',
+    fontFamily: 'Merienda-Regular',
+    color: '#484848',
+    flex: 1,
+    alignSelf: 'center',
+    marginTop: 100
+  }
+});
